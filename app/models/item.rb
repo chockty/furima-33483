df9be26b0a8d018720d4249cc1463da0189d5368
chockty <75976150+chockty@users.noreply.github.com>
2021-01-04 20:10:27 +0900

    itemモデルのvalidationとアソシエーションを編集
    itemsコントローラのrenderメソッドが一部誤りがあり修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_one_attached :image
+  validates :name, :info, :image, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range" }, format: {with: /\A[0-9]+\z/, message: "Half-width number"}
+  with_options numericality: { only_integer: true, other_than: 0, message: "Select" } do
+    validates :item_category_id, :item_sales_status_id, :item_shipping_id, :prefecture_id, :item_scheduled_delivery_id
+  end
+
   has_one :purchase
   belongs_to :user
-  belongs_to :item_category_id
-  belongs_to :item_shipping_id
-  belongs_to :prefecture_id
-  belongs_to :item_scheduled_delivery_id
+  belongs_to :item_category
+  belongs_to :item_sales_status
+  belongs_to :item_shipping
+  belongs_to :prefecture
+  belongs_to :item_scheduled_delivery
 end
