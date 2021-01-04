b78441dc1e5f67ef6b7fff6dc4b0475414fc2ca9
chockty <75976150+chockty@users.noreply.github.com>
2021-01-04 11:44:54 +0900

    userのvalidationを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z{6,}/i.freeze
-  validates :password, presence: true, format: { with: PASSWORD_REGEX,
-                                                 message: 'Include both letters and numbers' }
-  validates :nickname, presence: true
-  validates :first_name, :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/,
-                                                               message: 'Full-width characters' }
-  validates :first_reading_name, :last_reading_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,
-                                                                               message: 'Full-width katakana characters' }
-  validates :birth_date, presence: true
 
+  with_options presence: true do
+    validates :password, format: { with: PASSWORD_REGEX, message: 'Include both letters and numbers' }
+    validates :first_name, :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+    validates :first_reading_name, :last_reading_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+    validates :nickname, :birth_date
+  end
+  
   has_many :items
   has_many :purchases
 end
