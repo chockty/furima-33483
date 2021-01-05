0b124cbd4eaa519d7eb90138c43d0034c2c63c43
chockty <75976150+chockty@users.noreply.github.com>
2021-01-05 12:36:19 +0900

    itemモデルのvalidationを一部修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,9 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   has_one_attached :image
-  validates :name, :info, :image, presence: true
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range" }, format: {with: /\A[0-9]+\z/, message: "Half-width number"}
+  validates :name, :info, :image, :price, presence: true 
+  validates :price, numericality: { with: /\A[0-9]+\z/, message: "Half-width number"}
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range" }
   with_options numericality: { only_integer: true, other_than: 0, message: "Select" } do
     validates :item_category_id, :item_sales_status_id, :item_shipping_id, :prefecture_id, :item_scheduled_delivery_id
   end
