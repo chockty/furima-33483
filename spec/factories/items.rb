4410001016ad055c5c8d657930cec1c091bd51ef
chockty <75976150+chockty@users.noreply.github.com>
2021-01-05 12:35:51 +0900

    itemモデル用のfactorybotを準備

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Name.name}
+    info {Faker::Lorem.sentence}
+    item_category_id {Faker::Number.within(range: 1..10)}
+    item_sales_status_id {Faker::Number.within(range: 1..6)}
+    item_shipping_id {Faker::Number.within(range: 1..2)}
+    prefecture_id {Faker::Number.within(range: 1..47)}
+    item_scheduled_delivery_id {Faker::Number.within(range: 1..3)}
+    price {Faker::Number.within(range: 300..9999999)}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open("public/images/sample1.png"), filename: "sample1.png")
+    end
   end
 end
