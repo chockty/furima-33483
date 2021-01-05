5807f9c562fb0a2e5276d8b6d2f2093cdaaeb6e5
chockty <75976150+chockty@users.noreply.github.com>
2021-01-05 17:07:40 +0900

    price.jsの不要箇所を削除

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,7 +1,6 @@
 function price() {
   const itemPrice = document.getElementById("item-price");
   itemPrice.addEventListener("keyup", () => {
-    const itemPrice = document.getElementById("item-price");
     const taxPrice = document.getElementById("add-tax-price");
     const profitPrice = document.getElementById("profit");
     const taxValue = Math.floor(itemPrice.value / 10)
