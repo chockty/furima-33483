9a1f175266bbcd8c2628d11071c6b59796337779
chockty <75976150+chockty@users.noreply.github.com>
2021-01-07 16:56:38 +0900

    puchaseshippingaddressモデルを編集

--- app/models/purchase_shippingaddress.rb
+++ app/models/purchase_shippingaddress.rb
@@ -1,11 +1,16 @@
 class PurchaseShippingaddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :token               | string     | null: false                    |
+  attr_accessor :hoge, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id
   
   with_options presence: true do
-    validates :city, :addresses, :postal_code,
+    validates :city, :addresses, :postal_code
   end
   validates :postal_code, format:{with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
   validates :prefecture_id, numericality:{only_integer: true, other_than: 0, message: 'Select'}
   validates :phone_number, numericality:{with: /\A[0-9]+\z/, message: "Input only number"}
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Shippingaddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, purchase_id: purchase.id)
+  end
 end
