43ced30d837fc4fec813afbb3393d7cc3ec38737
chockty <75976150+chockty@users.noreply.github.com>
2021-01-08 18:35:50 +0900

    PurchaseShippingaddressモデルを編集
    ・クレジット決済用に「token」を追加
    ・phone_numberのvalidationを修正

--- app/models/purchase_shippingaddress.rb
+++ app/models/purchase_shippingaddress.rb
@@ -1,13 +1,13 @@
 class PurchaseShippingaddress
   include ActiveModel::Model
-  attr_accessor :hoge, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id, :token
   
   with_options presence: true do
-    validates :city, :addresses, :postal_code
+    validates :token, :postal_code, :city, :addresses, :phone_number, :user_id, :item_id
   end
-  validates :postal_code, format:{with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
+  validates :postal_code, format:{with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"} 
   validates :prefecture_id, numericality:{only_integer: true, other_than: 0, message: 'Select'}
-  validates :phone_number, numericality:{with: /\A[0-9]+\z/, message: "Input only number"}
+  validates :phone_number, format:{with: /\A[0-9]+\z/, message: "Input only number"}, length:{maximum: 11}
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
